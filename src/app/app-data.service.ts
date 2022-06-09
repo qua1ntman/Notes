ec642fd05a86c0b75780a1df5025b47c5eff452a
Mikhail Ashla <redikfrost@gmail.com>
2022-06-09 22:39:48 +0300

    feat: add service to get data from server and add interface

---
+++ src/app/app-data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Note } from './interfaces/note';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppDataService {
+  URL = 'http://localhost:3000/notes'
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.URL)
+  }
+
+  postData(data: Note[]) {
+    this.http.post(this.URL, {notes: data}).subscribe(data => console.dir(data))
+  }
+
+}
